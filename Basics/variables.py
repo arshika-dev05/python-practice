36708233c0bd9f89ba4dd10777f16992ad5a43ca
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-03 17:10:46 +0530

    variables.py

---
+++ Basics/variables.py
@@ -0,0 +1,13 @@
+# varibales prectice
+print("Variables prectice")
+student="Arshika"
+age=21
+name2="Akanksha" 
+print(f"my name is {student} and my age {age}.")
+print(name2)
+
+# reassigning a variable
+name="rohan"
+name="riya"
+name="raman"
+print(name)
