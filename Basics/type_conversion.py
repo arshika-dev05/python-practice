dad526bdcd44fb19f5752048ed043ed792e7ff9f
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-04 12:38:17 +0530

    type_conversion.py

---
+++ Basics/type_conversion.py
@@ -0,0 +1,16 @@
+# type_conversion
+# author: arshika
+# convert str into int,float 
+num1="20"
+num2="30"
+print(num1 + num2)
+print(int(num1) + int(num2))
+print(float(num1) + float(num2))
+
+# int into str 
+a=20
+b=123
+print(a+b)
+# 20 + 123=143
+print(str(a) + str(b))
+# 20123
