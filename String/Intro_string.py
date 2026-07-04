8e861c32aa2ab5c4f86c195db5ed9bdb9bf883f1
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-04 23:01:24 +0530

    Intro_string.py

---
+++ String/Intro_string.py
@@ -0,0 +1,15 @@
+# String Quotes
+# single Quotes(' ')
+# Double Quotes(" ")
+# Triple Quotes('''  ''') or ("""  """)
+
+name='Arshika'
+message ="Hii, I'm Arshika"
+paragraph ="""Future AI & ML engineer | Learning Python | Building Project Every Day"""
+
+print(name)
+print(message)
+print(paragraph)
+
+
+
