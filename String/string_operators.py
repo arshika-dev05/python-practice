c7f151bd5b62176df5d305cd2ca30f9228fbb07f
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-05 19:20:24 +0530

    string_operators.py

---
+++ String/string_operators.py
@@ -0,0 +1,14 @@
+# string Operators
+#  + concatenation
+#  * repeat
+#  in
+#  not in
+first_name ="Arshika"
+second_name ="Kumari"
+print(first_name +" " + second_name)
+print(first_name * 2)
+print(first_name * 3)
+print(first_name  in second_name)
+print(first_name  not in second_name)
+print(first_name  in "Arshika")
+print(second_name in "Arshika")
