24a4acaafa9c6e153c5d911e5980012a86acea99
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-05 19:02:27 +0530

    string_slicing

---
+++ String/string_slicing.py
@@ -0,0 +1,22 @@
+# string slicing
+# Rakesh kumar singh leatter - 16 , space - 2 length--18
+name="Rakesh kumar singh"
+print(len(name))
+print(name[0:1])
+print(name[0:2])
+print(name[0:3])
+print(name[0:4])
+print(name[0:5])
+print(name[0:6])
+print(name[0:7])
+print(name[0:8])
+print(name[0:9])
+print(name[0:10])
+print(name[0:12])
+print(name[0:13])
+print(name[0:14])
+print(name[0:15])
+print(name[0:16])
+print(name[0:17])
+print(name[0:18])
+ 
