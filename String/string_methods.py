58e1c1ee158aaaa59bc640d686903b94f1507fc9
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-06 15:53:10 +0530

    string_method.py

---
+++ String/string_methods.py
@@ -0,0 +1,30 @@
+# String methods
+name="arshika"
+print("uppercase=",name.upper())
+
+text="ARSHIKA"
+print("lowercase=",text.lower())
+
+message="my name is arshika"
+print("Title=",message.title())
+
+print("capitalize=",message.capitalize())
+
+word="   python    "
+print(word.strip())
+
+find="arshika"
+print(find.find("a"))
+
+replace="I love you"
+print(replace.replace("you","Dog"))
+
+index="python"
+print(index.index("h"))
+
+count="I love python"
+print("I love you count in 'o'=",count.count("o"))
+
+ 
+
+
