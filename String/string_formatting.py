da24b487cca3a4b3902f5697543feb302c8a791c
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-07 18:13:17 +0530

    string_formatting.py

---
+++ String/string_formatting.py
@@ -0,0 +1,7 @@
+# string formatting in python
+# f-string
+name="Arshika"
+age=21
+print(f"hii, my name is {name} and i am {age} years old.")
+print(f"name={name} \nage={age}")
+print(f"name {name},age {age} ")
