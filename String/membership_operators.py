e780c9f11d96039dc4ec8f46dad98ecf04851db9
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-07 18:23:54 +0530

    membership_operators.py

---
+++ String/membership_operators.py
@@ -0,0 +1,12 @@
+# membership operators in python 
+# 'in' or 'not in' 👀
+
+message="I love Python"
+print("Python" in message)
+# True
+print("Java" not in message)
+# True
+print("Java" in message)
+# False
+
+
