6cb0dcb5ab0f7230510a6056167c08cf3ccda756
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-10 16:10:36 +0530

    list_indexing.py

---
+++ list/list_indexing.py
@@ -0,0 +1,16 @@
+# Indexing in list 
+# positive Index
+# start=0 
+fruits=["apple","banana","mango"]
+print("Positive Indexing")
+print(f"lenght={len(fruits)}")
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(type(fruits))
+# negative Index
+# start=-1 
+print("Negative Indexing")
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
