7f87ae0ebf5012acabf07a2a0d33bf372e22334f
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-16 15:40:21 +0530

    count.py

---
+++ list/List_Method/count.py
@@ -0,0 +1,8 @@
+# count()
+print("How work count()👇")
+my_list=["Arshika","ravi","ravina","Arshika"]
+print("before =",my_list)
+count_list=my_list.count("Arshika")
+print("count =",count_list)
+
+ 
