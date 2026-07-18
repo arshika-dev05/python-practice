f5bed1338c4e33b4d9e665e27f793cd6bf2dc2ec
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-18 14:36:27 +0530

    intro_dictionary.py

---
+++ Dectionary/intro_dictionary.py
@@ -0,0 +1,9 @@
+# A dictionary is a built in data structure in python that stores data in key-value pair.
+# Key --> Used to identify the data.
+# Value --> The actual data stored.
+students={"name":"Arshika", 
+          "age": 20,
+          "roll":26,
+          }  
+print(students)
+ 
