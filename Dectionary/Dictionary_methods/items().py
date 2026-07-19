adf61030297bb58c824107ae1101462fbf667cdb
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-19 12:48:48 +0530

    items().py

---
+++ Dectionary/Dictionary_methods/items().py
@@ -0,0 +1,7 @@
+# the items() method return all key-value pairs as tuples.
+student={"name":"Arshika",
+         "age":20,
+         "course":"AI"
+         }
+print(student)
+print(student.items())
