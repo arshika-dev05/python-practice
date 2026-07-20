cb96c3883ede61799499b7cee1f9847d2d53a1cf
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-20 19:05:32 +0530

    dictsum.py

---
+++ Dectionary/dictsum.py
@@ -0,0 +1,11 @@
+# sum
+dict_num={1:20,
+          2:10,
+          3:30,
+          4:29
+          }
+all_values=dict_num.values()
+all_keys=dict_num.keys()
+print(all_values)
+print(all_keys)
+print(sum(all_values))
