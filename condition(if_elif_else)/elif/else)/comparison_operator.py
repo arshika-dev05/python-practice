213a65bab4fa6a523e64c9fc88a23bd6dc7bc57c
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-22 16:20:52 +0530

    if_elif_else.py

--- condition(if_elif_else)/elif/else)/comparison_operator.py
+++
@@ -1,8 +0,0 @@
-# comparison opetarors
-# relational(comparison) operators
-# == equal to
-# =! not equle to
-# >  greater than
-# <  less than
-# >= greter than or equle to
-# <= less than or equle to 
---
+++ condition(if_elif_else)/if_elif_else.py
@@ -0,0 +1,10 @@
+# if / elif / else
+age=int(input("enter your age="))
+if age<=3:
+    print("Sorry, babies🐣 under three are not allowed on this ride.")
+elif age < 12:
+    print("you are eligible for this ride , ride charge = 30💸")
+elif age < 18:
+    print("you are eligible for this ride, ride charge = 40 💸")
+else:
+    print("sorry, you are not eligible for this ride try another ride thank you")    
