1f21d05bb34883264aacc72c25e5620207ad885c
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-22 15:58:51 +0530

    if_else_condition.py

---
+++ condition(if_elif_else)/elif/else)/comparison_operator.py
@@ -0,0 +1,8 @@
+# comparison opetarors
+# relational(comparison) operators
+# == equal to
+# =! not equle to
+# >  greater than
+# <  less than
+# >= greter than or equle to
+# <= less than or equle to 
---
+++ condition(if_elif_else)/if_ilse_condition.py
@@ -0,0 +1,8 @@
+#if else
+print("enter your height for ride")
+height=int(input("enter height in cm ="))
+if height>120:
+    print("sorry, you are not eligible for ride")
+else:
+    print("wow, you are eligible for ride")
+         
